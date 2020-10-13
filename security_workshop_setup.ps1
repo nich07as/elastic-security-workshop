357131252bc1d2314f8613f9677379d22cf8132e
Nicholas Lim <nicholas.lim@elastic.co>
2020-10-13 11:55:23 +0800

    Update security_workshop_setup.ps1

--- security_workshop_setup.ps1
+++ security_workshop_setup.ps1
@@ -18,26 +18,26 @@ $beat_config_repository_uri = "https://raw.githubusercontent.com/nich07as/elasti
 Write-Output "*** Security Workshop Setup ***`n"
 
 #Install Sysmon
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$sysmon_installer_uri = "https://download.sysinternals.com/files/Sysmon.zip"
-$sysmon_config_uri = "https://raw.githubusercontent.com/olafhartong/sysmon-modular/master/sysmonconfig.xml"
-$sysmon_local_rules_filepath = "C:\Windows\sysmon.xml"
-if (Test-Path "C:\Windows\Sysmon64.exe")
-{
-    Write-Output "Uninstalling Sysmon..."
-    Start-Process -WorkingDirectory "C:\Windows" -FilePath "sysmon64" -ArgumentList "-u" -Wait -NoNewWindow
-}
-Write-Output "Installing Sysmon..."
-$sysmon_tmp_dir = "$install_dir\sysmon"
-if (!(Test-Path $sysmon_tmp_dir)) {
-    New-Item -Path $sysmon_tmp_dir -Type directory | Out-Null
-}
-Invoke-WebRequest -Uri $sysmon_config_uri -OutFile $sysmon_local_rules_filepath
-Invoke-WebRequest -Uri $sysmon_installer_uri -OutFile $sysmon_tmp_dir/Sysmon.zip
-Expand-Archive -Path $sysmon_tmp_dir/Sysmon.zip -DestinationPath $sysmon_tmp_dir
-Start-Process -WorkingDirectory $sysmon_tmp_dir -FilePath "sysmon64" -ArgumentList "-accepteula -i $sysmon_local_rules_filepath" -Wait -NoNewWindow
-Remove-Item -Path $sysmon_tmp_dir -Recurse -Force
-Write-Output "Sysmon Installation Complete"
+#[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+#$sysmon_installer_uri = "https://download.sysinternals.com/files/Sysmon.zip"
+#$sysmon_config_uri = "https://raw.githubusercontent.com/olafhartong/sysmon-modular/master/sysmonconfig.xml"
+#$sysmon_local_rules_filepath = "C:\Windows\sysmon.xml"
+#if (Test-Path "C:\Windows\Sysmon64.exe")
+#{
+#    Write-Output "Uninstalling Sysmon..."
+#    Start-Process -WorkingDirectory "C:\Windows" -FilePath "sysmon64" -ArgumentList "-u" -Wait -NoNewWindow
+#}
+#Write-Output "Installing Sysmon..."
+#$sysmon_tmp_dir = "$install_dir\sysmon"
+#if (!(Test-Path $sysmon_tmp_dir)) {
+#    New-Item -Path $sysmon_tmp_dir -Type directory | Out-Null
+#}
+#Invoke-WebRequest -Uri $sysmon_config_uri -OutFile $sysmon_local_rules_filepath
+#Invoke-WebRequest -Uri $sysmon_installer_uri -OutFile $sysmon_tmp_dir/Sysmon.zip
+#Expand-Archive -Path $sysmon_tmp_dir/Sysmon.zip -DestinationPath $sysmon_tmp_dir
+#Start-Process -WorkingDirectory $sysmon_tmp_dir -FilePath "sysmon64" -ArgumentList "-accepteula -i $sysmon_local_rules_filepath" -Wait -NoNewWindow
+#Remove-Item -Path $sysmon_tmp_dir -Recurse -Force
+#Write-Output "Sysmon Installation Complete"
 
 #Download Elastic Cloud Deployment Plan
 Write-Output "`nDownloading Elastic Cloud Deployment Plan..."
